b0937a95b8409434d8b0849d87f91dda517f9d2c
Neil <vitalene@msu.edu>
2016-08-22 23:35:24 -0400

    almost through with stage three

--- tinyAddressBook/main.m
+++ tinyAddressBook/main.m
@@ -11,10 +11,33 @@ int main(int argc, const char * argv[]) {
         
     }
     
-    Card *cardstuff = printCard();
+    NSMutableArray *rolodex = [NSMutableArray array];
+    Card *card1 = [[Card alloc] initWithName:@"George Jones" emailAddress:@"email@example.net" buddyName:@"Buddy Holly"];
+    Card *card2 = [[Card alloc] initWithName:@"Trisha Yearwood" emailAddress:@"email2@example.net" buddyName:@"Buddy Hackett"];
+    Card *card3 = [[Card alloc] initWithName:@"Cotton-Eye Joe" emailAddress:@"email3@example.net" buddyName:@"Buddy Rich"];
+
+  //  NSLog(@"%p", @[&card1];
     
-   // NSLog(@"This is the card's contents: %@", cardstuff);
+    int i;
+    for (i=0; i < 3; i++) {
+        switch (i) {
+            case 0:
+                card1 = printCard();
+                rolodex[i] = card1;
+                break;
+            case 1:
+                card2 = printCard();
+                rolodex[i] = card2;
+                break;
+            case 2:
+                card3 = printCard();
+                rolodex[i] = card3;
+                break;
+            default:
+                break;
+        }
+    }
     
-    
-    return 0;
+    NSLog(@"THIS IS YOUR ARRAY:  %@", rolodex);
+          return 0;
 }
